e4b490236dc07902b067e3702afad33127be4e5f
连广然 <lianguangran@gmail.com>
2022-11-06 22:39:36 +0800

    修改字体大小

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "WenQuanYi Micro Hei Mono:pixelsize=24:antialias=true:autohint=true";
+static char *font = "WenQuanYi Micro Hei Mono:pixelsize=20:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
